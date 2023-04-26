e21e212646ac65a228dd3943a15f217afcc58b46
Wruthless <wsgnzls@comcast.net>
2023-04-26 06:07:47 -0400

    Initial commit.

---
+++ src/test/java/com/wruthless/json/springbootmmjson/SpringBootMmJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wruthless.json.springbootmmjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMmJsonApplicationTests {
+
+    @Test
+    void contextLoads( ) {
+    }
+
+}
